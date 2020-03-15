2784a0ee73f5524a06771c6aaa66706c6db09733
Akash Gajjar <akashgajjar8@gmail.com>
2020-03-16 00:00:55 +0530

    update ORM

--- db.py
+++ db.py
@@ -1,29 +1,47 @@
 """ Our own tiny ORM """
 # using lightweight sqlite database, perfect for our purpose
 import sqlite3
-
-conn = sqlite3.connect("vocab.db")
-
+from datetime import datetime
+import time
 
 def create_words_table():
     try:
+        conn = sqlite3.connect("vocab.db")
         cursor = conn.cursor()
         cursor.execute('''
         CREATE TABLE IF NOT EXISTS WORDS
         (WORD           CHAR(50)        PRIMARY KEY     NOT NULL,
         ADDITION_DATE   INT             NOT NULL,
-        COUNT      INT             DEFAULT 1       NOT NULL);
+        COUNT           INT             DEFAULT 1);
         ''')
         print("Words table created successfully!")
+        conn.commit()
         cursor.close()
     except sqlite3.Error as e:
         print("some error occured:", e.args[0])
 
 def drop_words_table():
     try:
+        conn = sqlite3.connect("vocab.db")
         cursor = conn.cursor()
         cursor.execute("DROP TABLE IF EXISTS WORDS")
         print("Words table dropped successfully!")
-        cursor.close()
+        conn.commit()
+        conn.close()
+    except sqlite3.Error as e:
+        print("some error occured:", e.args[0])
+
+def insert_words(word_list):
+    try:
+        conn = sqlite3.connect("vocab.db")
+        # need to do this whole conversion cause we only want the UTC timestamp accurate to the date, not to the actual hours/seconds/miliseconds/...
+        todays_date = datetime.utcnow().strftime("%d/%m/%Y")
+        todays_utc_seconds = time.mktime(datetime.strptime(todays_date, "%d/%m/%Y").timetuple())
+        cursor = conn.cursor()
+        words = [ (word, todays_utc_seconds) for word in word_list ]
+        cursor.executemany('INSERT INTO WORDS (WORD, ADDITION_DATE) VALUES (?,?)', words)
+        print("Successfully inserted {} words in table".format(len(words)))
+        conn.commit()
+        conn.close()
     except sqlite3.Error as e:
         print("some error occured:", e.args[0])
--- master.py
+++ master.py
@@ -3,42 +3,74 @@ import sys
 import argparse
 import logging
 import requests
-from db import create_words_table, drop_words_table
+from re import match
+from db import create_words_table, drop_words_table, insert_words
 
 """ function to print the helper info which helps in choosing the right option """
 def get_user_input ():
-    print("""
-    Choose any of the below
-    ----------------------------
-    create: Create words table
-    drop: Drop words table
-    .exit: Exit the dungeon
-    """)
+    # if __verbose__:
+    #     print("""
+    #     Choose any of the below
+    #     ----------------------------
+    #     create: Create words table
+    #     drop: Drop words table
+    #     .exit: Exit the dungeon
+    #     """)
     inp = input("$: ")
     return inp
 
 """ function to evaluate and take action upon the input provided by user """
 def evaluate_user_input (option):
-    if "." in option:
-        process_meta_command(option)
-    else:
-        execute_command(option)
+    match_obj = match(r'^(\.?)(.*)$', option)
+    if match_obj:
+        is_meta = match_obj.group(1)
+        actual_command = match_obj.group(2)
+        if is_meta:
+            process_meta_command(actual_command)
+        else:
+            execute_command(actual_command)
 
 """ function to process meta commands like exit.. """
 def process_meta_command (command):
-    command = command[1:]
     if command == "exit":
         print ("goodbye")
         sys.exit(0)
     else:
         print ("invalid meta command")
 
+def execute_insert_input (arguments):
+    if arguments:
+        match_obj = match(r'^(.+\.txt)|(\d+)$', arguments)
+        if match_obj:
+            file_name = match_obj.group(1)
+            word_count = match_obj.group(2)
+            if file_name:
+                try:
+                    f = open(file_name, 'r')
+                    word_list = f.read().split("\n")
+                    insert_words(word_list)
+                    f.close()
+                except IOError as e:
+                    print("Error while reading from file")
+            else:
+                print(word_count)
+        else:
+            print("pleae provide arguments in valid format")
+    else:
+        print("insert command requires additional arguments. Usage: insert [filename.txt|wordcount]")
+
 """ function to execute the general commands like insert and create """
 def execute_command (command):
-    if command == "create":
-        create_words_table()
-    elif command == "drop":
-        drop_words_table()
+    match_obj = match(r'^(create|drop|insert)( (.*))?$', command)
+    if match_obj:
+        actual_command = match_obj.group(1)
+        additional_arguments = match_obj.group(3)
+        if actual_command == "create":
+            create_words_table()
+        elif actual_command == "drop":
+            drop_words_table()
+        elif actual_command == "insert":
+            execute_insert_input(additional_arguments)
     else:
         print("invalid operation")
 
@@ -49,6 +81,7 @@ def parse_cla ():
     args = parser.parse_args()
     if args.verbose:
         logging.basicConfig(level=logging.DEBUG)
+        __verbose__ = args.verbose
 
 def main ():
     print("welcome to the dungeon of words")
